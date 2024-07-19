22290c6b77a0f375e0a84673f4bc5bf77e416100
Mari-devop <mari.philon@gmail.com>
2024-07-19 15:56:04 +0300

    fix layout

--- src/pages/pages.styled.ts
+++ src/pages/pages.styled.ts
@@ -21,5 +21,19 @@ export const Container = styled.div`
                 text-decoration: underline;
             }
         }
-    }   
+    }  
+    button {
+        padding: 10px 20px;
+        background-color: var(--accent-color);
+        color: var(--primary-bg-color);
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+        font-size: 16px;
+        font-weight: bold;
+        transition: background-color 0.3s;
+        &:hover {
+            background-color: var(--primary-color);
+        }
+    }     
 `;
